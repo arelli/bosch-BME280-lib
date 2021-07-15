7e4367cf4fa66882e9ea79056e830abd9229a8ca
Raf Ellinitakis <nickpap556@gmail.com>
2021-07-15 11:38:40 +0300

    Update bme280_register_read.ino

--- bme280_register_read.ino
+++ bme280_register_read.ino
@@ -32,20 +32,22 @@
 #define chip_id 0xD0
 
 // the hardcoded i2c address of the sensor(found with arduino 12c scanner sketch)
-int BME280adress = 0x76;
+int BME280adress = 0x76 ;
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();  //initiate the wire library(needed!)
-  set_register(BME280adress,reset_register,B10110110);   //   B10110110 is the required sequence to reset the device
-  delay(100);
-  set_register(BME280adress,ctrl_meas, B00100101);  // do one forced measurement cycle, the only right way to address specific registers
+  
+//  set_register(BME280adress,reset_register,B10110110);   //   B10110110 is the required sequence to reset the device
+//  delay(100);
+  set_register(BME280adress,ctrl_meas, B00100111);  // do one forced measurement cycle, the only right way to address specific registers
   set_register(BME280adress,ctrl_hum ,B00000001);  // the humidity sensor needs to be activated(carefull:update after ctrl_meas!!)
+  set_register(BME280adress,ctrl_meas, B00100111);
 }
  
  
-int temp1,temp2;
-int pres1,pres2;
+int temp1,temp2, temp3;
+int pres1,pres2,pres3;
 int hum1, hum2;
 
  
@@ -54,60 +56,58 @@ void loop() {
   delay(50);  // wait for all the measurements to be done
   
   //initiate a transmission and  ask for the data from the following registers
-  read_register(BME280adress,ctrl_meas, "control measuremetn register", "BIN");
-  read_register(BME280adress,status_register, "status_register", "BIN");
-  read_register(BME280adress,ctrl_hum, "***ctrl_hum***", "BIN");
-  
-  temp1 = read_register(BME280adress,temperature_msb, "temperature_msb", "BIN");
-  temp2 = read_register(BME280adress,temperature_lsb, "temperature_lsb", "BIN");
-  unsigned int temperature = temp1 * 256 + temp2;
+  read_register(BME280adress,ctrl_meas, "control measuremetn register");
+  read_register(BME280adress,status_register, "status_register");
+  read_register(BME280adress,ctrl_hum, "***ctrl_hum***");
+
+  // Get the temperature******************************************
+  temp1 = read_register(BME280adress,temperature_msb, "temperature_msb");
+  temp2 = read_register(BME280adress,temperature_lsb, "temperature_lsb");
+  temp3 = read_register(BME280adress,temperature_xlsb, "temperature_xlsb");
+  temp3 = temp3 & 11110000;  // only the 4 first bits are used
+  //unsigned int temperature = temp1 * 256 + temp2;
+  int temperature = temp1;
+  temperature = (temperature << 8) | temp2;
+  temperature = (temperature << 4) | temp3;
   float temperature_float = float(temperature)/1000;
-  Serial.print("\nTemp.:");
-  Serial.print(temperature_float,4);  //print only 4 decimals
-  Serial.print("°C,\n ");
+  Serial.print("Temp.:"); Serial.print(temperature_float,4); Serial.print("°C,\n ");
 
-  hum1 = read_register(BME280adress,humidity_msb, "humidity_msb", "BIN");
-  hum2 = read_register(BME280adress,humidity_lsb, "humidity_lsb", "BIN");
+  // Get the humidity*********************************************
+  hum1 = read_register(BME280adress,humidity_msb, "humidity_msb");
+  hum2 = read_register(BME280adress,humidity_lsb, "humidity_lsb");
   unsigned int humidity = float(hum1 *256 + hum2)/1024;  // humidity is in Q10.22 format -> divide by 1024
-  Serial.print("\nHum.: ");
-  Serial.print(humidity, DEC);
-  Serial.print("% RH, \n");
-
-  pres1 = read_register(BME280adress,pressure_msb, "pressure_msb", "BIN");
-  pres2 = read_register(BME280adress,pressure_lsb, "pressure_lsb", "BIN");
-  read_register(BME280adress,pressure_xlsb, "pressure_xlsb", "BIN");
+  Serial.print("Hum.: "); Serial.print(humidity, DEC); Serial.print("% RH, \n");
+  
+  // Get the pressure*********************************************
+  pres1 = read_register(BME280adress,pressure_msb, "pressure_msb");
+  pres2 = read_register(BME280adress,pressure_lsb, "pressure_lsb");
+  pres3 = read_register(BME280adress,pressure_xlsb, "pressure_xlsb");
+  pres3 = pres3 & 11110000;
   float pressure = float(pres1 + float(pres2)/256);  // is represented in Q24.8 format  -> divide by 256
-  Serial.print("\nPres.: ");
-  Serial.print(pressure, 4);
-  Serial.print(" Pa \n");
+  Serial.print("Pres.: "); Serial.print(pressure, 4);Serial.print(" Pa \n");
  
   delay(10000);
 
   // force mode, make 1 measurement the next loop
-  set_register(BME280adress,ctrl_meas, B00100101);  
+  //set_register(BME280adress,ctrl_meas, B00100101);  
 }
 
 
-int read_register(int ic_address,int reg_address, String reg_name, String readmode){
+int read_register(int ic_address,int reg_address, String reg_name){
+  int returned_value;
   Wire.beginTransmission(ic_address);
   Wire.write(reg_address);
-  int returned_value;
   
   Wire.requestFrom(ic_address,byte(1));
-  if( Wire.available() == 1){  // check if there are 2 available bytes
+  if( Wire.available() == 1){  
     Serial.print(reg_name);
     Serial.print(" = ");
     returned_value = Wire.read();
     Wire.endTransmission();
-    if (readmode=="BIN")
-      Serial.print(returned_value, BIN);
-    else if (readmode=="DEC")
-     Serial.print(returned_value, DEC);
-    else if (readmode=="HEX")
-       Serial.print(returned_value, HEX);
+    Serial.print(returned_value, BIN);
   }
   else
-    Serial.println( "requestFrom failed");
+    return -1;
 
   Serial.print("\n");
   return returned_value;
